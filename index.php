41a3f302137e2c40bdfb2030e90a7573e27a23f6
Amritgrg1 <gamrit450@gmail.com>
2022-09-15 21:28:14 +0545

    check

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8"> <!-- try -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
@@ -17,7 +17,7 @@
   <thead>
     <tr>
       <th scope="col">ID</th>
-      <th scope="col">TODO Item </th>
+      <th scope="col">TODO Item </th> 
       <th scope="col">Action</th>
     </tr>
   </thead>
